2905053cbb5d1f562c80e8a948ed9ad019092a11
chanda811 <puniarare.dopuni@gmail.com>
2021-03-30 11:20:48 +0900

    最初のコミット

---
+++ up/select_image.php
@@ -0,0 +1,25 @@
+<?php
+mb_internal_encoding("utf8");
+
+$pdo = new PDO("mysql:dbname=lesson01;host=localhost;","root","");
+$result = $pdo->query("select image from img_upload;");
+
+foreach($result as $row){
+    $uploaded_img = $row['image'];
+}
+
+$image_path="./image/".$uploaded_img;
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <title>画像アップロード</title>
+    </head>
+
+    <body>
+        <img src="<?php echo $image_path;?>">
+    </body>
+</html>
